e84e9c178c4b29bb7ad340f2b606c286ebed8d5d
Kristen Liu <Kristen@kristens-mbp-2.mynetworksettings.com>
2024-07-18 13:46:02 -0400

    Initial commit for marble solitaire project

---
+++ marble_solitaire/src/MarbleSolitaire.java
@@ -0,0 +1,17 @@
+package cs3500.marblesolitaire;
+
+/**
+ * Represents what the users input for which board they want to play the Solitaire game on.
+ */
+public final class MarbleSolitaire {
+
+  /**
+   * The maine method that starts the game.
+   * @param args The arguments that are passed in by the user.
+   */
+  public static void main(String[] args) {
+    for (String s: args) {
+      System.out.println(s);
+    }
+  }
+}
---
+++ marble_solitaire/src/controller/MarbleSolitaireController.java
@@ -0,0 +1,12 @@
+package cs3500.marblesolitaire.controller;
+
+/**
+ * Represents the controller object for the game.
+ */
+public interface MarbleSolitaireController {
+
+  /**
+   * Plays a new game of Marble Solitaire.
+   */
+  public void playGame() throws IllegalArgumentException;
+}
